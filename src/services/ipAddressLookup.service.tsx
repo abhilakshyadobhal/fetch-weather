37f27077b0278598de484bbb4a82af3f80639e47
Abhilakshya Dobhal <abhilakshyadobhal@gmail.com>
2020-05-24 14:22:01 +0530

    fixed cors error

--- src/services/ipAddressLookup.service.tsx
+++ src/services/ipAddressLookup.service.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getLocationFromIp = async () => {
-  const res = await axios.get('http://ip-api.com/json');
+  const res = await axios.get('https://ip-api.com/json');
   return res.data;
 };
