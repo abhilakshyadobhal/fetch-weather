903c6553b9f682cec94dc4e5a9e375008c72609f
Abhilakshya Dobhal <abhilakshyadobhal@gmail.com>
2020-05-25 13:21:27 +0530

    removed border line

--- src/app/component/HourlyData/HourlyData.tsx
+++ src/app/component/HourlyData/HourlyData.tsx
@@ -58,6 +58,7 @@ const HourlyData: React.FC<IProps> = ({ hourlyData }) => {
                     },
                     gridLines: {
                       display: false,
+                      drawBorder: false,
                     },
                   },
                 ],
@@ -70,6 +71,7 @@ const HourlyData: React.FC<IProps> = ({ hourlyData }) => {
                     },
                     gridLines: {
                       display: true,
+                      drawBorder: false,
                     },
                   },
                 ],
