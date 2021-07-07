7bad5b4c8e364143848f398d4ed1ab1bc9fc0bd5
ABHILAKSHYA DOBHAL <abhilakshyadobhal@gmail.com>
2021-07-07 10:51:31 +0530

    Update InputComponent.tsx

--- src/app/component/InputComponent/InputComponent.tsx
+++ src/app/component/InputComponent/InputComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.scss';
 import { getCityWeather } from '../../../services/weather.service';
 import { getError } from '../../utils';
